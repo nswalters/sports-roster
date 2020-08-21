1e14373c28902787cf6ff91c04bdf935828d517c
Nick Walters <nswalters.s@gmail.com>
2020-08-20 19:17:30 -0500

    Initialize firebase connection;Update auth state on login and logout button clicks

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../../helpers/data/connection';
+
+fbConnection();
 
 class Auth extends React.Component {
   state = {
     authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(googleProvider);
+  }
+
+  logoutClickEvent = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
   }
 
   render() {
@@ -11,11 +42,11 @@ class Auth extends React.Component {
     const authBasedComponent = () => {
       if (authed) {
         return (
-          <button className="btn btn-danger ml-auto">Logout</button>
+          <button className="btn btn-danger ml-auto" onClick={this.logoutClickEvent}>Logout</button>
         )
       }
       return (
-        <button className="btn btn-warning ml-auto">Login</button>
+        <button className="btn btn-warning ml-auto" onClick={this.loginClickEvent}>Login</button>
       )
     }
 
