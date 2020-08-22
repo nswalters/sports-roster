9eab7a2e34e38cbedc8a45bf7af3f9d209b4b4a7
Nick Walters <nswalters.s@gmail.com>
2020-08-22 12:28:15 -0500

    Added addPlayer function

--- src/helpers/data/playerData.js
+++ src/helpers/data/playerData.js
@@ -14,4 +14,6 @@ const getPlayersByUid = (uid) => new Promise((resolve, reject) => {
 
 const deletePlayer = (playerId) => axios.delete(`${baseUrl}/players/${playerId}.json`);
 
-export default { getPlayersByUid, deletePlayer };
+const addPlayer = (newPlayerObj) => axios.post(`${baseUrl}/players.json`, newPlayerObj);
+
+export default { addPlayer, deletePlayer, getPlayersByUid };
