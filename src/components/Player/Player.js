6eb12cc47599b9fbbe2bc5fcce9278911c698e7e
Nick Walters <nswalters.s@gmail.com>
2020-08-22 09:09:12 -0500

    Updated styling

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import playerShape from '../../helpers/propTypes/playerShape';
 
+import './Player.scss';
+
 class Player extends React.Component {
   static propTypes = {
     player: playerShape.playerShape,
@@ -11,7 +13,7 @@ class Player extends React.Component {
     const { player } = this.props;
 
     return (
-      <div className="card text-center shadow">
+      <div className="card text-center shadow player-card">
         <div className="card-header"><h5>{player.name}</h5></div>
         <img className="card-img w-50 shadow rounded-circle mt-3" src={player.imageUrl} alt="Player"></img>
         <div className="card-body">
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,6 +5,8 @@ import Player from '../Player/Player';
 import authData from '../../helpers/data/authData';
 import playerData from '../../helpers/data/playerData';
 
+import './Team.scss';
+
 class Team extends React.Component {
   state = {
     players: [],
@@ -22,8 +24,17 @@ class Team extends React.Component {
     const playerCards = players.map((player) => <Player key={player.id} player={ player } />);
 
     return (
-      <div className="card-columns">
-        {playerCards}
+      <div>
+        <h1 className="font-weight-bold mt-5 mb-3">Meet Your Team</h1>
+        <img className="mx-auto team-logo"src="https://www-league.nhlstatic.com/nhl.com/builds/site-core/b1904568bcb2fbac5e1943f4bbcc4a888f9cd027_1596505987/images/logos/team/current/team-18-dark.svg" alt="Team Logo"></img>
+        <h3 className="font-weight-bold my-3">The Nashville Predators</h3>
+        <h6 className="mt-2 mb-3">They call Bridgestone Arena their home.</h6>
+        <small className="team-notes">2017 Stanley Cup Finalists</small>
+        <div className="container mt-5">
+          <div className="card-columns">
+            {playerCards}
+          </div>
+        </div>
       </div>
     )
   }
