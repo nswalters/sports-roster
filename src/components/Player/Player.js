c616272591754bac50cc0406eb4cca8301033f7a
Nick Walters <nswalters.s@gmail.com>
2020-08-22 10:29:25 -0500

    Added delete player event and supporting functions and proptypes

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import playerShape from '../../helpers/propTypes/playerShape';
 
@@ -7,6 +8,13 @@ import './Player.scss';
 class Player extends React.Component {
   static propTypes = {
     player: playerShape.playerShape,
+    deletePlayer: PropTypes.func.isRequired
+  }
+
+  deletePlayerEvent = (e) => {
+    e.preventDefault();
+    const { player, deletePlayer } = this.props;
+    deletePlayer(player.id);
   }
 
   render() {
@@ -16,7 +24,7 @@ class Player extends React.Component {
       <div className="card text-center shadow player-card">
         <div className="card-header d-flex flex-nowrap justify-content-center">
           <h5 className="ml-auto mb-0">{player.name}</h5>
-          <button type="button" className="close ml-auto text-danger delete-player-button" aria-label="Delete Player">&times;</button>
+          <button type="button" onClick={this.deletePlayerEvent} className="close ml-auto text-danger delete-player-button" aria-label="Delete Player">&times;</button>
         </div>
         <img className="card-img w-50 shadow rounded-circle mt-3" src={player.imageUrl} alt="Player"></img>
         <div className="card-body">
