61c5efba1d5aa1ee1b57c22b56c94a5b9bdcb267
Nick Walters <nswalters.s@gmail.com>
2020-08-22 10:17:57 -0500

    Add delete button to player card

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -14,7 +14,10 @@ class Player extends React.Component {
 
     return (
       <div className="card text-center shadow player-card">
-        <div className="card-header"><h5>{player.name}</h5></div>
+        <div className="card-header d-flex flex-nowrap justify-content-center">
+          <h5 className="ml-auto mb-0">{player.name}</h5>
+          <button type="button" className="close ml-auto text-danger delete-player-button" aria-label="Delete Player">&times;</button>
+        </div>
         <img className="card-img w-50 shadow rounded-circle mt-3" src={player.imageUrl} alt="Player"></img>
         <div className="card-body">
           <p className="card-title">{player.position}</p>
